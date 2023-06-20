064b8440e639678485df39d748e40a431cde7f3a
Underline007 <99379498+Underline007@users.noreply.github.com>
2023-06-20 09:41:46 +0700

    first commit

---
+++ TrainingApp/screens/Login.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import {
+  Image,
+  ImageBackground,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+function Login(props) {}
+return <View>Login</View>;
+
+export default Login;
